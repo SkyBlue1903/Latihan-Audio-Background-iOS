9047235b50fa6ade0fce3503cb4a04a10f3be3f9
Erlangga Anugrah Arifin <angga1433@gmail.com>
2022-09-19 18:34:46 +0700

    Inisialisasi

--- Latihan Audio Background/ViewController.swift
+++ Latihan Audio Background/ViewController.swift
@@ -6,14 +6,55 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var playButton: UIButton!
+    @IBOutlet weak var stopButton: UIButton!
+    
+    private var player: AVAudioPlayer?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        isPlaying(state: false)
+        
+        guard let url = Bundle.main.url(forResource: "PianoLoop", withExtension: "mp3") else {
+              return
+            }
+
+        do {
+              try AVAudioSession.sharedInstance().setCategory(.playback, mode: .default)
+
+              // MARK: Kode ini untuk iOS 11 ke atas.
+              player = try AVAudioPlayer(contentsOf: url, fileTypeHint: AVFileType.mp3.rawValue)
+                    
+              // MARK: Kode ini untuk iOS 10 ke bawah.
+              // player = try AVAudioPlayer(contentsOf: url, fileTypeHint: AVFileTypeMPEGLayer3)
+                    
+            } catch let error {
+              print(error.localizedDescription)
+            }
     }
 
-
+    @IBAction func playMusic(_ sender: UIButton) {
+        guard let audioPlayer = player else { return }
+        audioPlayer.play()
+        isPlaying(state: true)
+    }
+    
+    
+    @IBAction func stopMusic(_ sender: UIButton) {
+        guard let audioPlayer = player else { return }
+        audioPlayer.stop()
+        isPlaying(state: false)
+    }
+    
+    // Memperbaiki Status Tombol Play dan Stop
+    private func isPlaying(state: Bool) {
+      stopButton.isEnabled = state
+      playButton.isEnabled = !state
+    }
+    
 }
 
